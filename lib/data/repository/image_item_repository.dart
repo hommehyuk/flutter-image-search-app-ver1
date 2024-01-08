16620dc53a9bf0667f2844cbdfb5f80505f13a1c
hommehyuk <bin6827@gmail.com>
2024-01-08 18:47:18 +0900

    feat: 가짜 데이터 동작 되는 것 처럼 속이기

--- lib/data/repository/image_item_repository.dart
+++ lib/data/repository/image_item_repository.dart
@@ -1,23 +1,36 @@
 import 'package:flutter_image_search_app_ver1/data/model/imate_item.dart';
 
-class ImageItemRepository {
+class MockImageItemRepository {
   Future<List<ImageItem>> getImageItems(String query) async {
-    return [
-      ImageItem(
-          imageUrl:
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb4A9XVQkuLtniqhGli-'
-                  'Juq8Sa-4fcN4AATA&usqp=CAU',
-          tags: ''),
-      ImageItem(
-          imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?'
-              'q=tbn:ANd9GcQJnA83PbFf1jpRT6acEr0Lofe52-ULCFsuUQ&usqp=CAU',
-          tags: ''),
-      ImageItem(
-          imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tb'
-              'n:ANd9GcRn7YkCXnDQKx9W8VdKHfp_GaQZ7nsbKKg0Kw&usqp=CAU',
-          tags: ''),
-    ];
+    await Future.delayed(const Duration(seconds: 1));
+
+    if (query == 'haerin') {
+      return [
+        ImageItem(
+            imageUrl:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb4A9XVQkuLtniqhGli-'
+                'Juq8Sa-4fcN4AATA&usqp=CAU',
+            tags: ''),
+        ImageItem(
+            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?'
+                'q=tbn:ANd9GcQJnA83PbFf1jpRT6acEr0Lofe52-ULCFsuUQ&usqp=CAU',
+            tags: ''),
+        ImageItem(
+            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tb'
+                'n:ANd9GcRn7YkCXnDQKx9W8VdKHfp_GaQZ7nsbKKg0Kw&usqp=CAU',
+            tags: ''),
+      ];
+    } else {
+      return [
+        ImageItem(
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2015/03/14/19/45/suit-673697_150.jpg",
+            tags: ''),
+        ImageItem(
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2019/09/21/09/07/banana-4493420_150.jpg",
+            tags: ''),
+      ];
+    }
   }
 }
