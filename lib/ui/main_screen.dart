6d7c5aaf1a3608dd7ca33cd9174028da52031d1a
hommehyuk <bin6827@gmail.com>
2024-01-08 18:24:29 +0900

    feat: UI 디자인, 모델 완성

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/model/imate_item.dart';
+import 'package:flutter_image_search_app_ver1/ui/widget/image_item_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -7,20 +9,54 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const TextField(),
-            Expanded(
-              child: GridView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return const Text('111');
-                },
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: const BorderSide(
+                      width: 2,
+                      color: Color(0xFF4FB6B2),
+                    ),
+                  ),
+                  hintText: 'Search',
+                  suffixIcon: const Icon(
+                    Icons.search,
+                    color: Color(0xFF4FB6B2),
+                  ),
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 16),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    final imageItem = ImageItem(
+                      imageUrl:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTlyQmzyfV-r9wkynwoft-ivRyMIYe0Q_CnA&usqp=CAU',
+                      tags: 'haerin',
+                    );
+                    return ImageItemWidget(imageItem: imageItem);
+                  },
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 32,
+                    mainAxisSpacing: 32,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/ui/widget/image_item_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_search_app_ver1/data/model/imate_item.dart';
+
+class ImageItemWidget extends StatelessWidget {
+  final ImageItem imageItem;
+
+  const ImageItemWidget({
+    super.key,
+    required this.imageItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: Image.network(
+        imageItem.imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
